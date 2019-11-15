22bd7e5831aa7b540c3ca3e674113788c05c426b
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-15 15:27:32 -0600

    Se instaló la dependencia nodemon y el servidor acepta peticiones get y delete en formato JSON o enoded. Se terminó la clase: 'Recibir información desde el cliente: Body y Query'.

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
 
 var app = express()
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(router)
 
 router.get('/message', function (req, rest) {
     rest.send('lista de mensajes')
 })
 
-router.post('/message', function (req, rest) {
-    rest.send('Mensaje añadido')
+router.delete('/message', function (req, rest) {
+    console.log(req.query)
+    console.log(req.body)
+    rest.send('Mensaje ' + req.body.text + ' añadido correctamente')
 })
 
 // app.use('/', function (req, res) {
