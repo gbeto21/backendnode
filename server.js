39742f15f830e5175d1a84ea4badcc1b6810e48a
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-14 21:23:49 -0600

    Se añadieron formas de recibir peticiones get, post y delete en el archivo server.js. Se terminó la clase: '¿Cómo pueden venir las peticiones?'

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const router = express.Router()
 
 var app = express()
 
-app.use('/', function (req, res) {
-    res.send('Hola')
+app.use(router)
+
+router.get('/message', function (req, rest) {
+    rest.send('lista de mensajes')
 })
+
+router.post('/message', function (req, rest) {
+    rest.send('Mensaje añadido')
+})
+
+// app.use('/', function (req, res) {
+//     res.send('Hola')
+// })
 
 app.listen(3000)
 console.log('La aplicación está escuchano en http://localhost:3000')
